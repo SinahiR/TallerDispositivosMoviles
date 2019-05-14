f783101d79951ea1ce4d526e87fc784580160652
Sinahi Ruiz <siruizro@ittepic.edu.mx>
2019-05-13 20:06:18 -0500

    Taller de dispositivos moviles
    Taller de dispositivos moviles

---
+++ Unidad5/51ListaSensores_SinahiR/app/build/generated/source/buildConfig/debug/com/example/a51listasensores_sinahir/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.a51listasensores_sinahir;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.a51listasensores_sinahir";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
